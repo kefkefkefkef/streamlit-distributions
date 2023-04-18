3383972d4f314d2b46bc895709947eccc87b3090
kef <kekekefir@gmail.com>
2023-04-18 16:16:28 +0300

    fdsf

--- dist.py
+++ dist.py
@@ -22,15 +22,15 @@ st.pyplot(fig1)
 
 #x = np.linspace(-10,10,100)
 k = np.arange(11)
-lam = st.slider(label='input mean', min_value=-5., max_value=5., value=4.)
-mass_poi = st.poisson(lam).pmf(k)
-cumul_poi = st.poisson(lam).cdf(k)
+lam = 3#st.slider(label='input lam', min_value=-5., max_value=5., value=4.)
+mass_poi = sct.poisson(lam).pmf(k)
+cumul_poi = sct.poisson(lam).cdf(k)
 fig2, ax2 = plt.subplots()
 plt.vlines(k, [0]*len(mass_poi), mass_poi)#, label='Probability mass function of Poi')
 plt.title('Количество людей заходящих в магазин в минуту')
 plt.xlabel('Количество людей')
 plt.ylabel('Вероятность')
-
+st.pyplot(fig2)
 #mean2 = st.slider(label='input mean', min_value=-5., max_value=5., value=4.)
 #sd2 = st.slider(label='input std dev', min_value=0.1, max_value=5., value=4.)
  
@@ -40,7 +40,7 @@ plt.ylabel('Вероятность')
 #plt.plot(x, pdf2, color = 'blue')
 #plt.xlabel('Data points')
 #plt.ylabel('Probability Density')
-st.pyplot(fig2)
+
 
 #x = np.linspace(-10,10,100)
   
